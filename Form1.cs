b563c0e9c3957c006e2eb0a32c842ee02b4b4563
Mohammad-Safwan <msafwan678@gmail.com>
2020-07-15 22:04:12 +0500

    Integrated the whole thing

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using SudokuSolverWindowsForms.Helpers;
+using SudokuSolverWindowsForms.Strategies;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,7 +15,13 @@ namespace SudokuSolverWindowsForms
 {
     public partial class Form1 : Form
     {
+
         OpenFileDialog openFile = new OpenFileDialog();
+        SudokuMapper sudokuMapper;
+        SudokuStateManager sudokuStateManager;
+        SudokuSolverEngine sudokuSolverEngine;
+        SudokuFileInput sudokuFileInput;
+        
         int[,] sudokuBoard;
         public Form1()
         {
@@ -23,7 +30,10 @@ namespace SudokuSolverWindowsForms
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            /**/
+            sudokuMapper = new SudokuMapper();
+            sudokuStateManager = new SudokuStateManager();
+            sudokuSolverEngine = new SudokuSolverEngine(sudokuStateManager, sudokuMapper);
+            sudokuFileInput = new SudokuFileInput();
         }
 
         private void BtnFileUpload_Click(object sender, EventArgs e)
@@ -32,10 +42,12 @@ namespace SudokuSolverWindowsForms
             //openFile.
             if(openFile.ShowDialog() == DialogResult.OK)
             {
-                SudokuFileInput sudokuFileInput = new SudokuFileInput();
                 sudokuBoard = sudokuFileInput.ReadFile(openFile.FileName);
-                //MessageBox.Show(sudokuBoard[0,0].ToString() + " " + sudokuBoard[7,7].ToString());
-                DisplayOnForm(sudokuBoard, 15);
+                DisplayOnForm(sudokuBoard, 0, "Initial");
+                bool isSudokuSolved = sudokuSolverEngine.Solve(sudokuBoard);
+                DisplayOnForm(sudokuBoard, 15, "Solved");
+                MessageBox.Show(isSudokuSolved ? "You have successfully solved the puzzle" : "Unfortunately current algorithms were not sufficient to solve the sudoku puzzle");
+
             }
             else
             {
@@ -44,7 +56,7 @@ namespace SudokuSolverWindowsForms
             }
         }
 
-        public void DisplayOnForm(int[,] board, int position)
+        public void DisplayOnForm(int[,] board, int position, string title)
         {
             Label[,] labels = new Label[9, 9];
 
@@ -53,7 +65,7 @@ namespace SudokuSolverWindowsForms
                 for (int j = 0; j < 9; j++)
                 {
                     labels[i, j] = new Label();
-                    labels[i, j].Location = new Point((i + 2 + position) * 30, (j + 2) * 30);
+                    labels[i, j].Location = new Point((i + 2 + position) * 30, (j + 4) * 30);
                     labels[i, j].Width = 20;
                     labels[i, j].Height = 20;
                     labels[i, j].ForeColor = Color.White;
@@ -63,6 +75,15 @@ namespace SudokuSolverWindowsForms
                     this.Controls.Add(labels[i, j]);
                 }
             }
+
+            Label titleForSudoku = new Label();
+            titleForSudoku.Location = new Point((position+1) * 30, (position + 50) * 30);
+            titleForSudoku.Width = 20;
+            titleForSudoku.Height = 20;
+            titleForSudoku.ForeColor = Color.Blue;
+            titleForSudoku.Text = title;
+            titleForSudoku.TextAlign = ContentAlignment.MiddleCenter;
+            this.Controls.Add(titleForSudoku);
         }
     }
 }
