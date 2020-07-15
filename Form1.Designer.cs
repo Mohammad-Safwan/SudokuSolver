d58c0632c952fb39a13a5e18e5d08d64673eda30
Mohammad-Safwan <msafwan678@gmail.com>
2020-07-15 15:41:04 +0500

    Project setup with file picker

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SudokuSolverWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnFileUpload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnFileUpload
+            // 
+            this.BtnFileUpload.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BtnFileUpload.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.BtnFileUpload.FlatAppearance.BorderSize = 2;
+            this.BtnFileUpload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFileUpload.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFileUpload.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.BtnFileUpload.Location = new System.Drawing.Point(254, 12);
+            this.BtnFileUpload.Name = "BtnFileUpload";
+            this.BtnFileUpload.Size = new System.Drawing.Size(243, 56);
+            this.BtnFileUpload.TabIndex = 0;
+            this.BtnFileUpload.Text = "Click To Select Text File";
+            this.BtnFileUpload.UseVisualStyleBackColor = false;
+            this.BtnFileUpload.Click += new System.EventHandler(this.BtnFileUpload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnFileUpload);
+            this.Name = "Form1";
+            this.Text = "SudokuSolverBackground";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnFileUpload;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuSolverWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFile = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*Label[,] labels = new Label[9, 9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    labels[i, j] = new Label();
+                    //labels[i, j].Top = 20;
+                    //labels[i, j].Left = 30;
+                    labels[i, j].Location = new Point((i + 2) * 30, (j + 2) * 30);
+                    labels[i, j].Width = 20;
+                    labels[i, j].Height = 20;
+                    labels[i, j].BackColor = (i >= 0 && i<3 && j>=0 && j<3) ? SystemColors.ActiveCaption : SystemColors.ActiveCaption;
+                    labels[i, j].Text = String.Format("{0}", i + j);
+                    labels[i, j].TextAlign = ContentAlignment.MiddleCenter;
+                    this.Controls.Add(labels[i, j]);
+                }
+            }*/
+        }
+
+        private void BtnFileUpload_Click(object sender, EventArgs e)
+        {
+            openFile.Filter = "Text files (*.txt)|*.txt";
+            //openFile.
+            openFile.ShowDialog();
+        }
+    }
+}
